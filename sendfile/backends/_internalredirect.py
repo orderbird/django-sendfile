4dd30eb029a899157c7280ae6ef2e8ed745aec42
Michael Handerek <michael.handerek@orderbird.com>
2016-08-10 15:15:33 +0200

    Add propper escaping of urls

--- sendfile/backends/_internalredirect.py
+++ sendfile/backends/_internalredirect.py
@@ -1,16 +1,18 @@
 from django.conf import settings
 import os.path
 
+from django.utils.encoding import escape_uri_path
+
 
 def _convert_file_to_url(filename, remote_storage=False):
     url = [settings.SENDFILE_URL]
 
     if remote_storage:
         url.insert(1, filename)
+        return escape_uri_path(u'/'.join(url))
     else:
         relpath = os.path.relpath(filename, settings.SENDFILE_ROOT)
         while relpath:
             relpath, head = os.path.split(relpath)
             url.insert(1, head)
-
-    return u'/'.join(url)
+        return u'/'.join(url)
